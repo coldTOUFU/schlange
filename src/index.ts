0146291fef09a6b9ac7c9b745bf0dd533a44e806
coldTOUFU <coldtoufu.dev@gmail.com>
2022-12-28 15:45:05 +0900

    change: 引数の数を多くしたくないので、drachenブランチではdrachenを実行するようにした。

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,15 @@ import { UnoClient } from "./client";
 import { UnoPlayerInterface } from "./player_interface";
 
 const process = require("process");
-const isConnectionTestMode = (process.argv.length === 4 && process.argv[3] === "test");
-let player: UnoPlayerInterface.PlayerInterface;
+const isConnectionTestMode = (process.argv.length === 3 && process.argv[2] === "test");
 if (isConnectionTestMode) {
   process.env.HOST = "http://localhost:3000";
   process.env.DEALER = "TestDealer";
   process.env.PLAYER = "TestPlayer1";
-  if (process.argv[2] === "Schlange") {
-    player = new Schlange();
-  } else {
-    player = new Drachen();
-  }
 } else if (process.argv.length === 5) {
   process.env.HOST = process.argv[2];
   process.env.DEALER = process.argv[3];
   process.env.PLAYER = process.argv[4];
-  if (process.argv[2] === "Schlange") {
-    player = new Schlange();
-  } else {
-    player = new Drachen();
-  }
 } else {
   console.error("引数(対戦): HOSTNAME DEALER_NAME PLAYER_NAME");
   console.error("  e.g.: node out/index.js http://localhost:8080 Dealer Player01");
@@ -32,6 +21,7 @@ if (isConnectionTestMode) {
   process.exit(1);
 }
 
+const player = new Drachen();
 const client = new UnoClient.Client(process.env.HOST,
                                     process.env.DEALER,
                                     process.env.PLAYER,
